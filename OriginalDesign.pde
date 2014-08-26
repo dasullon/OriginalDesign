886a98c39161bae22d68eef3ecfbf9a7e0817490
dasullon <dasullon@697-334-Linhir.sf.k12.internal>
2014-08-26 11:30:22 -0700

    Added first name to index.html

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,18 +1,41 @@
-void setup()
+  int x= 128;
+  int y= 0;
+  int z= 255;
+  
+  
+  void setup()
 {
   size(300,300);
   background(0);
+  
 }
 void draw()
 {
   fill(0,0,0,5);
-  stroke(0,0,0,5);
+  stroke(0,0,0,100);
   rect(0,0,300,300);
   noFill();
-  stroke(0,255,0);
+  stroke(x,y,z);
   translate(150,150);
-  rotate(random(4*PI));
+  rotate(random(PI));
   ellipse(0,0,250,random(250));
+  if (x<255){
+  x++;
+  }
+  if (x==255){
+     x=floor (random(0, 256));
+  }
+  if (y<255){
+  y++;
+  }
+  if (y==255){
+     y=floor (random(0, 256));
+  }
+
+ if (z<255){
+  z++;
+  }
+  if (z==255){
+     z=floor(random(0, 256));
+  }
 }
-
-
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,15 +14,47 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
-public void setup()
+  int x= 128;
+  int y= 0;
+  int z= 255;
+  
+  
+  public void setup()
 {
-
+  size(300,300);
+  background(0);
+  
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
-}
+  fill(0,0,0,5);
+  stroke(0,0,0,100);
+  rect(0,0,300,300);
+  noFill();
+  stroke(x,y,z);
+  translate(150,150);
+  rotate(random(PI));
+  ellipse(0,0,250,random(250));
+  if (x<255){
+  x++;
+  }
+  if (x==255){
+     x=floor (random(0, 256));
+  }
+  if (y<255){
+  y++;
+  }
+  if (y==255){
+     y=floor (random(0, 256));
+  }
 
+ if (z<255){
+  z++;
+  }
+  if (z==255){
+     z=floor(random(0, 256));
+  }
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
